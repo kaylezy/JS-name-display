f5e887ede4f21cfa5147b16f6da02fdd9693a97d
Olakunle Hassan <31227288+kaylezy@users.noreply.github.com>
2022-06-24 11:17:07 +0100

    JS name display

---
+++ index.js
@@ -0,0 +1,66 @@
+
+
+// //   name display by id
+      function sayHello() {
+        let name = window.input.value;
+        document.getElementById(
+          "show"
+        ).innerHTML = `My name is  Olakunle Hassan <br> My height is 5ft 8 <br> I'm from Nigeria`;
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   //Display in the console
+//   let name = "Olakunle Hassan";
+//   let height = "5ft 8";
+//   let country = "I'm from Nigeria";
+
+//   console.log(name, height, country);
+
+
+
+
+
+
+
+
+// //   name display with prompt
+// let name;
+// name = prompt("Enter Your Name");
+// document.write("<h1>Hi " + name);
+
+
+
+
+
+
+
+
+
+
+
+
+// //   display on prompt with document.write
+// let name
+// let height = 1.7
+// let country
+// name = prompt("Enter your name")
+// height = prompt(`my height is: ${height}`)
+// country = prompt("Country name")
+// document.write(name + "<br>" + height + "<br>"+ country)
